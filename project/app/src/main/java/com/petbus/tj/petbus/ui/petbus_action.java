426f05dc049215bc032b1e80066bedb751c82698
terrycheng <terry.cheng@vip.163.com>
2019-05-16 03:18:00 +0800

    TerryCheng20190516:Restruct the gui as new one.

--- project/app/src/main/java/com/petbus/tj/petbus/ui/petbus_action.java
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/petbus_action.java
@@ -1,5 +1,5 @@
 package com.petbus.tj.petbus.ui;
-//https://blog.csdn.net/chaoshenzhaoxichao/article/details/79871145
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.view.LayoutInflater;
 
 import com.petbus.tj.petbus.middleware.middleware;
 import com.petbus.tj.petbus.middleware.middleware_impl;
@@ -21,90 +22,111 @@ public class petbus_action extends FragmentActivity implements OnClickListener {
     private Fragment m_action_fragment_diary;
     private Fragment m_action_fragment_alarm;
 
-
-    private ImageButton m_button_actionrecode;
-    private ImageButton m_button_actionoverview;
-    private ImageButton m_button_actiondiary;
-    private ImageButton m_button_actionalarm;
+    private ImageButton m_button_actionbutton;
+    private ImageButton m_button_overviewbutton;
+    private ImageButton m_button_settingbutton;
+    private ImageButton m_button_addbutton;
+    private TextView m_title_view;
 
     private middleware m_middleware;
 
+
+    private static final int MAINFRAMGENT_ID = 0;
+    private static final int OVERVIEWFRAMGENT_ID = 1;
+    private static final int SETTINGFRAMGENT_ID = 2;
+    private static final int PETMANAGEFRAMGENT_ID = 3;
+    private int m_fragment_id = MAINFRAMGENT_ID;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.petbus_action);
 
         m_middleware = middleware_impl.getInstance();
 
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setOnClickListener(this);
-
-        init_button();
+        init_view();
         active_fragment( 0 );
     }
 
-    public void onClick(View view) {
+    public void onClick( View view ) {
         Log.i( "PetBusApp", "PetBus:onClick" );
         switch( view.getId() )
         {
-            case R.id.sample_text:
-                Intent intent = new Intent();
-                intent.setClass(petbus_action.this, petbus_welcome.class);
-                startActivity(intent);
-                break;
-            case R.id.action_recode:
+            case R.id.leftreturn_button:
                 Log.i( "PetBusApp", "PetBus:action_recode" );
-                active_fragment(0);
+                active_fragment(MAINFRAMGENT_ID);
                 break;
-            case R.id.action_overview:
-                active_fragment(1);
-                Log.i( "PetBusApp", "PetBus:action_overview" );
+            case R.id.overview_button:
+                active_fragment(OVERVIEWFRAMGENT_ID);
+                Log.i( "PetBusApp", "PetBus:overview_button" );
                 break;
-            case R.id.action_diary:
-                active_fragment(2);
-                Log.i( "PetBusApp", "PetBus:action_overview" );
-                break;
-            case R.id.action_alarm:
-                active_fragment(3);
-                Log.i( "PetBusApp", "PetBus:action_overview" );
+            case R.id.setting_button:
+                active_fragment(SETTINGFRAMGENT_ID);
+                Log.i( "PetBusApp", "PetBus:setting_button" );
                 break;
         }
 
         m_middleware.get_petnumber();
     }
 
-    private void init_button()
+    private void init_view()
     {
-        m_button_actionrecode = (ImageButton) findViewById(R.id.action_recode);
-        m_button_actionoverview = (ImageButton) findViewById(R.id.action_overview);
-        m_button_actiondiary = (ImageButton) findViewById(R.id.action_diary);
-        m_button_actionalarm = (ImageButton) findViewById(R.id.action_alarm);
+        m_title_view = (TextView) findViewById( R.id.title_text );
+        m_button_actionbutton = (ImageButton) findViewById(R.id.leftreturn_button);
+        m_button_overviewbutton = (ImageButton) findViewById(R.id.overview_button);
+        m_button_settingbutton = (ImageButton) findViewById(R.id.setting_button);
+        m_button_addbutton = (ImageButton) findViewById(R.id.add_button);
 
-        m_button_actionrecode.setOnClickListener(this);
-        m_button_actionoverview.setOnClickListener(this);
-        m_button_actiondiary.setOnClickListener(this);
-        m_button_actionalarm.setOnClickListener(this);
+        m_button_actionbutton.setOnClickListener(this);
+        m_button_overviewbutton.setOnClickListener(this);
+        m_button_settingbutton.setOnClickListener(this);
+        m_button_addbutton.setOnClickListener(this);
+
+        m_button_actionbutton.setVisibility(View.INVISIBLE);
+        m_button_overviewbutton.setVisibility(View.INVISIBLE);
+        m_button_settingbutton.setVisibility(View.INVISIBLE);
+        m_button_addbutton.setVisibility(View.INVISIBLE);
     }
 
-    private void active_fragment(int i) {
+    private void reset_actionbutton( int id ){
+        m_button_actionbutton.setVisibility(View.INVISIBLE);
+        m_button_overviewbutton.setVisibility(View.INVISIBLE);
+        m_button_settingbutton.setVisibility(View.INVISIBLE);
+        m_button_addbutton.setVisibility(View.INVISIBLE);
+
+        switch( id ) {
+            case MAINFRAMGENT_ID:
+                m_title_view.setText( R.string.petdiary );
+                m_button_overviewbutton.setVisibility(View.VISIBLE);
+                m_button_settingbutton.setVisibility(View.VISIBLE);
+                break;
+            case OVERVIEWFRAMGENT_ID:
+                m_title_view.setText( R.string.overview );
+                m_button_actionbutton.setVisibility(View.VISIBLE);
+                break;
+            case SETTINGFRAMGENT_ID:
+                m_button_actionbutton.setVisibility(View.VISIBLE);
+                m_title_view.setText( R.string.settting );
+                break;
+        }
+    }
+
+    private void active_fragment( int i ) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         hide_fragment( transaction );
         switch (i) {
-            case 0:
+            case MAINFRAMGENT_ID:
                 if (m_action_fragment_recode == null) {
                     m_action_fragment_recode = new actionfragment_actionrecode();
-                    //m_action_fragment_recode.setOnClickListener(this);
                     transaction.add(R.id.fragment, m_action_fragment_recode);
                 } else {
                     transaction.show(m_action_fragment_recode);
                 }
                 break;
-            case 1:
+            case OVERVIEWFRAMGENT_ID:
                 if ( m_action_fragment_overview == null )
                 {
                     m_action_fragment_overview = new actionfragment_overview();
-                    // m_action_fragment_overview.setOnClickListener(this);
                     transaction.add(R.id.fragment, m_action_fragment_overview);
                 }
                 else
@@ -112,7 +134,7 @@ public class petbus_action extends FragmentActivity implements OnClickListener {
                     transaction.show( m_action_fragment_overview );
                 }
                 break;
-            case 2:
+            case SETTINGFRAMGENT_ID:
                 if ( m_action_fragment_diary == null )
                 {
                     m_action_fragment_diary = new actionfragment_diary();
@@ -123,43 +145,13 @@ public class petbus_action extends FragmentActivity implements OnClickListener {
                     transaction.show( m_action_fragment_diary );
                 }
                 break;
-            case 3:
-                if ( m_action_fragment_alarm == null )
-                {
-                    m_action_fragment_alarm = new actionfragment_alarm();
-                    transaction.add(R.id.fragment, m_action_fragment_alarm);
-                }
-                else
-                {
-                    transaction.show( m_action_fragment_alarm );
-                }
-                break;
 
         }
         transaction.commit();
+        reset_actionbutton( i );
+        m_fragment_id = i;
     }
 
-    private void reset_actionbutton( int id )
-    {
-        switch (id) {
-            case 0:
-                m_button_actionrecode.setImageResource( R.mipmap.recode_pressed );
-                break;
-            case 1:
-                m_button_actionoverview.setImageResource( R.mipmap.overview_pressed );
-                break;
-            case 2:
-                m_button_actiondiary.setImageResource( R.mipmap.diary_pressed );
-                break;
-            case 3:
-                m_button_actionalarm.setImageResource( R.mipmap.alarm_presssed );
-                break;
-        }
-        m_button_actionrecode.setImageResource( R.mipmap.recode_normal );
-        m_button_actionoverview.setImageResource( R.mipmap.overview_normal );
-        m_button_actiondiary.setImageResource( R.mipmap.diary_normal );
-        m_button_actionalarm.setImageResource( R.mipmap.alarm_normal );
-    }
 
     private void hide_fragment(FragmentTransaction transaction) {
         if (m_action_fragment_recode != null) {
@@ -174,7 +166,6 @@ public class petbus_action extends FragmentActivity implements OnClickListener {
         if (m_action_fragment_alarm != null) {
             transaction.hide(m_action_fragment_alarm);
         }
-
     }
 
 }
