a00e8ac454bf04fed9229809bd9fdfb830409736
terrycheng <terry.cheng@vip.163.com>
2019-05-25 20:58:26 +0800

    TeryrCheng20190525:1. implate the mainframe.2.add the overview.

--- project/app/src/main/java/com/petbus/tj/petbus/ui/action_fragment/actionfragment_actionrecode.java
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/action_fragment/actionfragment_actionrecode.java
@@ -2,18 +2,147 @@ package com.petbus.tj.petbus.ui;
 
 import com.petbus.tj.petbus.ui.R;
 import android.os.Bundle;
+import android.util.Log;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.petbus.tj.petbus.middleware.middleware;
+import com.petbus.tj.petbus.middleware.middleware_impl;
+
+//https://www.jianshu.com/p/26ce4fdf8210
+//https://blog.csdn.net/ghd2000/article/details/47663227?_t_t_t=0.11722124961357339
+class action_recode{
+    public action_recode( int id, int type ){
+        m_id = id;
+        m_type = type;
+    }
+
+    int get_recode_type(){
+        return m_type;
+    }
+    int get_recode_id(){
+        return m_id;
+    }
+    
+    private int m_id;
+    private int m_type;
+}
+
+class recode_daily_listview extends ArrayAdapter<action_recode> {
+    public static final int RECODE_TYPE_DATE = 0;
+    public static final int RECODE_TYPE_RECODE = 1;
+
+    private middleware m_middleware;
+    private int resourceID;
+    private Context m_Context;
+    private DisplayMetrics mDisplayMetrics;
+    private List<action_recode> mList;
+
+    public recode_daily_listview(Context context, int textViewResourceId,List<action_recode>objects){
+        super(context,textViewResourceId,objects);
+        m_Context = context;
+        resourceID = textViewResourceId;
+        mDisplayMetrics = m_Context.getResources().getDisplayMetrics();
+        m_middleware = middleware_impl.getInstance();
+        mList = objects;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        action_recode recode = getItem(position);
+        Log.i( "PetBusApp", "PetBus:getView " + recode );
+
+        view_holder_date date_view = null;
+        view_holder_recode recode_view = null;
+
+        if( convertView == null ){
+            switch( recode.get_recode_type() ){
+                case RECODE_TYPE_DATE:
+                    date_view = new view_holder_date();
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.recode_date
+                                                                           , parent, false);
+                    TextView date = (TextView) convertView.findViewById(R.id.daily_text);
+                    convertView.setTag(R.layout.recode_date, date_view);
+                    date.setText( "2019052" + recode.get_recode_id() + " ÐÇÆÚXX" );
+                    break;
+                case RECODE_TYPE_RECODE:
+                    recode_view = new view_holder_recode();
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.recode_recode
+                                                                           , parent, false); 
+                    convertView.setTag(R.layout.recode_recode, recode_view); 
+                    TextView name = (TextView) convertView.findViewById(R.id.recode_times);
+                    // name.setText( "Test-" + recode.get_recode_id() );
+                    break;
+            }
+        } else {
+            switch( recode.get_recode_type() ){
+                case RECODE_TYPE_DATE:
+                    date_view = (view_holder_date)convertView.getTag(R.layout.recode_date);
+                    break;
+                case RECODE_TYPE_RECODE:
+                    recode_view = (view_holder_recode)convertView.getTag(R.layout.recode_recode);
+                    break;
+            }
+        }
+
+        return convertView;
+    }
+
+    private class view_holder_date {
+    }
+    private class view_holder_recode{
+    }
+} 
 
 public class actionfragment_actionrecode extends Fragment
 {
+    private List<action_recode> m_daily_recode_list = new ArrayList<>();
+    private middleware m_middleware;
+
+    private void init_list_data(){
+        int i = 0;
+
+        for( i = 0;i < 7;i += 3  )
+        {
+            action_recode tmp = new action_recode( i, 0 );
+            m_daily_recode_list.add(tmp);
+            tmp = new action_recode( i + 1, 1 );
+            m_daily_recode_list.add(tmp);
+            tmp = new action_recode( i + 2, 1 );
+            m_daily_recode_list.add(tmp);
+        }
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState){
-        // TODO Auto-generated method stub return inflater.inflate(R.layout.fragment1, container, false);
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState ){
+        m_middleware = middleware_impl.getInstance();
+        init_list_data();
+
         View view = inflater.inflate(R.layout.actionfragment_actionrecode, container, false);
+        ArrayAdapter<action_recode> adapter = new recode_daily_listview(this.getActivity(),
+                R.layout.recode_daily_recode,m_daily_recode_list);
+ 
+        ListView listView =(ListView)view.findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        Log.i( "PetBusApp", "PetBus:getView " + m_daily_recode_list );
+
+        // listView.setOnItemClickListener(
+        //     new AdapterView.OnItemClickListener()
+        //         {
+        //             @Override public void onItemClick(AdapterView<?> parent, View view, final int position, long id)
+        //             {
+        //             }
+        //         });
         return view;
     }
 }
--- project/app/src/main/java/com/petbus/tj/petbus/ui/action_fragment/actionfragment_overview.java
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/action_fragment/actionfragment_overview.java
@@ -1,19 +1,108 @@
 package com.petbus.tj.petbus.ui;
 
 import com.petbus.tj.petbus.ui.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//ÏÂÀ­
+//https://www.cnblogs.com/guichun/p/4352546.html
+class action_adapter extends BaseAdapter {
+    private List<String> mList;
+    private Context mContext;
+ 
+    public action_adapter(Context pContext, List<String> pList) {
+        this.mContext = pContext;
+        this.mList = pList;
+    }
+ 
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+ 
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+ 
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater _LayoutInflater = LayoutInflater.from(mContext);
+        convertView = _LayoutInflater.inflate(R.layout.actionfragment_overview_selectitem, null);
+        if( convertView != null )
+        {
+            TextView _TextView1=(TextView)convertView.findViewById(R.id.action_text);
+            // TextView _TextView2=(TextView)convertView.findViewById(R.id.textView2);
+            _TextView1.setText(mList.get(position));
+            // _TextView2.setText(mList.get(position).getPersonAddress());
+        }
+        return convertView;
+    }
+}
 
 public class actionfragment_overview extends Fragment
 {
+    private ArrayList<Double> m_ylist;
+
+    private Spinner m_spinner;
+    private List<String> m_data_list;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState){
         // TODO Auto-generated method stub return inflater.inflate(R.layout.fragment1, container, false);
         View view = inflater.inflate(R.layout.actionfragment_overview, container, false);
+        m_spinner = (Spinner) view.findViewById(R.id.spinner);
+        LineGraphicView tabview = (LineGraphicView) view.findViewById(R.id.tabview_id);
+
+        m_ylist = new ArrayList<Double>();
+        m_ylist.add((double) 2.103);
+        m_ylist.add(4.05);
+        m_ylist.add(6.60);
+        m_ylist.add(3.08);
+        m_ylist.add(4.32);
+        m_ylist.add(2.0);
+        m_ylist.add(5.0);
+
+        ArrayList<String> xRawDatas = new ArrayList<String>();
+        xRawDatas.add("05-19");
+        xRawDatas.add("05-20");
+        xRawDatas.add("05-21");
+        xRawDatas.add("05-22");
+        xRawDatas.add("05-23");
+        xRawDatas.add("05-24");
+        xRawDatas.add("05-25");
+        xRawDatas.add("05-26");
+        tabview.setData(m_ylist, xRawDatas, 8, 2);
+
+
+        m_data_list = new ArrayList<String>();
+        m_data_list.add("Î¹Ê³");
+        m_data_list.add("²ùÊº");
+        m_data_list.add("Ï´Ôè");
+
+        action_adapter arr_adapter = new action_adapter(this.getActivity(), m_data_list);
+        // arr_adapter.setDropDownViewResource( R.layout.actionfragment_overview_selectitem );
+        m_spinner.setAdapter(arr_adapter);
         return view;
     }
 }
