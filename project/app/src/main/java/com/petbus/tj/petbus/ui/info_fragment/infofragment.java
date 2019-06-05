ffa54ee54da98b4929295d93cc6771ba36f109aa
antiwey <antiwey@google.com>
2019-06-05 21:09:24 +0800

    antiwey@gmail.com20190605:增加第一次进入APP时，需要显示的宠物添加页面。

---
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/info_fragment/infofragment.java
@@ -0,0 +1,20 @@
+package com.petbus.tj.petbus.ui.info_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.petbus.tj.petbus.ui.R;
+
+public class infofragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.petbus_firstvisit, container, false);
+        return view;
+    }
+}
---
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/petbus_firstvisit.java
@@ -0,0 +1,13 @@
+package com.petbus.tj.petbus.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+public class petbus_firstvisit extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.petbus_firstvisit);
+    }
+}
