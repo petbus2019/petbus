7c73975aafa8d34d7cab3031ba9e047db0845b3a
nineteenwj@gmail.com <nineteenwj@gmail.com>
2019-06-08 22:58:24 +0800

    antiwey@gmail.com20190608:解决启动添加宠物页面，APP crash的问题

--- project/app/src/main/java/com/petbus/tj/petbus/ui/info_fragment/infofragment.java
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/info_fragment/infofragment.java
@@ -14,7 +14,7 @@ public class infofragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.petbus_firstvisit, container, false);
+        View view = inflater.inflate(R.layout.infofragment, container, false);
         return view;
     }
 }
--- project/app/src/main/java/com/petbus/tj/petbus/ui/petbus_firstvisit.java
+++ project/app/src/main/java/com/petbus/tj/petbus/ui/petbus_firstvisit.java
@@ -3,11 +3,56 @@ package com.petbus.tj.petbus.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class petbus_firstvisit extends FragmentActivity {
+    Button btnNext;
+    TextView name, birth, weight;
+    RadioGroup gender, species;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.petbus_firstvisit);
+
+        btnNext = findViewById(R.id.id_nextBtn);
+        name = findViewById(R.id.id_inputName);
+        birth = findViewById(R.id.id_inputBirth);
+        weight = findViewById(R.id.id_inputWeight);
+        gender = findViewById(R.id.id_inputGender);
+        species = findViewById(R.id.id_inputSpecies);
+        btnNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String strBirth, strName, strWeight;
+                String strGender="NULL";
+                String strSpecies="NULL";
+                strBirth = birth.getText().toString();
+                strName = name.getText().toString();
+                strWeight = weight.getText().toString();
+                for (int i=0; i<gender.getChildCount();i++){
+                    RadioButton r=(RadioButton)gender.getChildAt(i);
+                    if(r.isChecked()){
+                        strGender = (String) r.getText();
+                        break;
+                    }
+                }
+                for (int i=0; i<gender.getChildCount();i++){
+                    RadioButton r=(RadioButton)species.getChildAt(i);
+                    if(r.isChecked()){
+                        strSpecies = (String) r.getText();
+                        break;
+                    }
+                }
+                Toast.makeText(petbus_firstvisit.this, strName+','+strBirth+","
+                        +strWeight+","+strGender+","+strSpecies, Toast.LENGTH_LONG ).show();
+            }
+        });
     }
 }
+
