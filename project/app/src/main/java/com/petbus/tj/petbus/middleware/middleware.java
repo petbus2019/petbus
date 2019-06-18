79335cf47146e8ce79aeee0f9ea68386a835000d
terrycheng <terry.cheng@vip.163.com>
2019-06-18 10:24:00 +0800

    TerryCheng20190618:重新提交被覆盖的代码.

--- project/app/src/main/java/com/petbus/tj/petbus/middleware/middleware.java
+++ project/app/src/main/java/com/petbus/tj/petbus/middleware/middleware.java
@@ -11,9 +11,9 @@ public interface middleware{
     public static final int RECORD_TYPE_DATE = 0;
     public static final int RECORD_TYPE_RECORD = 1;
 
-    public static final String DATE_FORMAT_FULL = "yyyy-MM-dd hh:mm:ss";
+    public static final String DATE_FORMAT_FULL = "yyyy-MM-dd HH:mm:ss";
     public static final String DATE_FORMAT_DATE = "yyyy-MM-dd";
-    public static final String DATE_FORMAT_TIME = "hh:mm:ss";
+    public static final String DATE_FORMAT_TIME = "HH:mm:ss";
 
     int get_petnumber();
 
